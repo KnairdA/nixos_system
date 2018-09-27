55e423c75bceb69ac40ef0741ce3679946f54ec6
Adrian Kummerlaender <knairda@t-online.de>
2018-09-27 16:01:34 +0200

    Use tree_content-provided Nix derivation

--- host/software/tree.kummerlaender.eu.nix
+++ host/software/tree.kummerlaender.eu.nix
@@ -1,40 +1,11 @@
 { pkgs, ... }:
 
 let
-  content = pkgs.fetchgit {
+  result = import (pkgs.fetchgit {
     url    = "gitea@localhost:web/tree_content.git";
-    rev    =  "bc3ac4828b8980dd83963f2cf2c4df8f10a8f96a";
-    sha256 = "0f7mgm8z714k34dc7v7iv5i6dmgmk4dyawbyx7y7p8cbjcg1bdw4";
-  };
-
-  result = pkgs.stdenv.mkDerivation {
-    name = "tree.kummerlaender.eu";
-
-    src = pkgs.fetchFromGitHub {
-      owner  = "KnairdA";
-      repo   = "tree.kummerlaender.eu";
-      rev    = "95f100c7ac9fba6116f0e268950b83b706325bf2";
-      sha256 = "0llffkjdz3x0yp3nvb1fa6041rj696xnkifvy6kalsbrxy8mbfn5";
-    };
-
-    LANG = "en_US.UTF-8";
-
-    buildInputs = let
-      mypkgs = import (fetchTarball "https://pkgs.kummerlaender.eu/nixexprs.tar.gz") { };
-    in [
-      pkgs.pandoc
-      pkgs.highlight
-      mypkgs.katex-wrapper
-      mypkgs.make-xslt
-    ];
-
-    installPhase = ''
-      cp -r ${content} source/00_content
-      make-xslt
-      mkdir $out
-      cp -Lr target/99_result/* $out
-    '';
-  };
+    rev    = "72ebbabe1466d2e7ec3f7b6035bf78dc88bdc35a";
+    sha256 = "0dqlqrz8wlgqkrqsn0fk861j81msrprmh7k1q733smgrykmr2v09";
+  }) { };
 in {
   services.nginx.virtualHosts."tree.kummerlaender.eu" = {
     addSSL = true;
