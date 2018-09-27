654548a491c8558f7643b853752654db4b6e9cc3
Adrian Kummerlaender <knairda@t-online.de>
2018-09-27 16:39:13 +0200

    Use blog_content-provided Nix derivation

--- host/software/blog.kummerlaender.eu.nix
+++ host/software/blog.kummerlaender.eu.nix
@@ -1,36 +1,12 @@
 { pkgs, ... }:
 
 let
-  result = pkgs.stdenv.mkDerivation {
-    name = "blog.kummerlaender.eu";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "KnairdA";
-      repo  = "blog.kummerlaender.eu";
-      fetchSubmodules = true;
-      rev    = "92beb2254a441d43bb66ab89a179853ccbe6bceb";
-      sha256 = "1mbxjkh8wxsvqb3yc6cirr3rady756vvys3v5l8k2rxkkl90wg1m";
-    };
-
-    LANG = "en_US.UTF-8";
-
-    buildInputs = let
-      mypkgs = import (fetchTarball "https://pkgs.kummerlaender.eu/nixexprs.tar.gz") { };
-    in [
-      pkgs.pandoc
-      pkgs.highlight
-      mypkgs.katex-wrapper
-      mypkgs.make-xslt
-    ];
-
-    patches = [ ./asset/blog.kummerlaender.eu-change-domain.patch ];
-
-    installPhase = ''
-      make-xslt
-      mkdir $out
-      cp -Lr target/99_result/* $out
-    '';
-  };
+  result = import (pkgs.fetchFromGitHub {
+    owner = "KnairdA";
+    repo  = "blog_content";
+    rev    = "c467ca2df2759a00edc4b15f81097299771fcd5c";
+    sha256 = "0kinmswnklpvmkaaks7x9gj8yk217299d9rc4ca7kdklmi4xsfrw";
+  }) { };
 in {
   services.nginx.virtualHosts."blog.kummerlaender.eu" = {
     addSSL = true;
