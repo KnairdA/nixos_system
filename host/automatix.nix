a9e707c85dcfaaee28657346426cab6a87516ced
Adrian Kummerlaender <knairda@t-online.de>
2018-10-04 19:32:58 +0200

    Configure Hetzner IPv6 on automatix

--- host/automatix.nix
+++ host/automatix.nix
@@ -23,6 +23,17 @@
     # open Nginx ports, other services open their own ports
       allowedTCPPorts = [ 80 443 ];
     };
+
+  # setup IPv6
+    interfaces.ens3.ipv6.addresses = [ {
+      address = "2a01:4f8:c010:f56::1";
+      prefixLength = 64;
+    } ];
+
+    defaultGateway6 = {
+      address   = "fe80::1";
+      interface = "ens3";
+    };
   };
 
   services = {
