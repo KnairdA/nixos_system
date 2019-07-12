5e0bcefa419499f9c7b89c242c25e7a4ef6da18f
Adrian Kummerlaender <knairda@t-online.de>
2019-07-12 21:10:02 +0200

    Enable NAT for wireguard server

--- host/software/server/wireguard.nix
+++ host/software/server/wireguard.nix
@@ -3,6 +3,8 @@
     allowedUDPPorts = [ 54321 ];
   };
 
+  networking.nat.enable = true;
+
   networking.wireguard.interfaces = {
     wg0 = {
       ips = [ "10.100.0.1/24" ];
@@ -14,15 +16,15 @@
       peers = [
         { # obelix
           publicKey = "RrsNZKZ17Ol1WHxZesLnenGKnqxiQlE0T8xFP6/5mBE=";
-          allowedIPs = [ "10.100.0.2/32" ];
+          allowedIPs = [ "10.100.0.2" ];
         }
         { # majestix
           publicKey = "Tkoaewh9HB5rIuJVrFgClRF4x7prOtIlSJjiTYCpxis=";
-          allowedIPs = [ "10.100.0.3/32" ];
+          allowedIPs = [ "10.100.0.3" ];
         }
         { # athena
           publicKey = "t4SzRV/olVdzAKauJOwFau3I0fTISUvbOAaKGZd6ezU=";
-          allowedIPs = [ "10.100.0.4/32" ];
+          allowedIPs = [ "10.100.0.4" ];
         }
       ];
     };
