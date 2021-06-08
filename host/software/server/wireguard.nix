249aec089fd9d27ea8f2a34a120e9ce43ca184f9
Adrian Kummerlaender <knairda@t-online.de>
2021-06-08 10:00:34 +0200

    Add new host to mesh

--- host/software/server/wireguard.nix
+++ host/software/server/wireguard.nix
@@ -34,6 +34,10 @@
           publicKey = "jUtbAF3TZDEFXlL+YTV3g26wP0IWGbpiCFGXjxo5TXE=";
           allowedIPs = [ "10.100.0.6" ];
         }
+        { # hephaestus
+          publicKey = "0nd/5vZaerTCUpS6uXsulCTzI3ZsUT2N2pnh7zTo8wg=";
+          allowedIPs = [ "10.100.0.7" ];
+        }
       ];
     };
   };
