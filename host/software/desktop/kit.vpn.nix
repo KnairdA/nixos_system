74a4d31089d4410204a97d87673413ec138de3c9
Adrian Kummerlaender <adrian@kummerlaender.eu>
2023-12-01 10:29:44 +0100

    Update openvpn config

--- host/software/desktop/kit.vpn.nix
+++ host/software/desktop/kit.vpn.nix
@@ -3,25 +3,13 @@
 {
   services.openvpn.servers = {
     KIT = {
-      # adapted from https://www.scc.kit.edu/scc/net/openvpn/os/debian/kit.ovpn
-      config = ''
-        client
-        remote 141.52.8.20
-        port 1194
-        dev tun
-        proto udp
-        auth-user-pass
-        nobind
-        comp-lzo no
-        tls-version-min 1.2
-        ca ${./asset/T-TeleSec_GlobalRoot_Class_2.pem}
-        verify-x509-name "C=DE, ST=Baden-Wuerttemberg, L=Karlsruhe, O=Karlsruhe Institute of Technology, CN=ovpn.scc.kit.edu" subject
-        cipher AES-256-CBC
-        auth SHA384
-        verb 3
-        script-security 2
-      '';
       autoStart = false;
+      config = let
+        path = pkgs.fetchurl {
+          url = "https://www.scc.kit.edu/scc/net/openvpn/conf/kit-split.ovpn";
+          hash = "sha256-j4pCKyU7t1ZmwIGm5kuUgZ26Qiqa1jzoDZcP2x+A9pM=";
+        };
+      in "config ${path}";
     };
   };
 }
