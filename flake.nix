8edf2d881db240eec8517a6511916a7eca66a5f8
Adrian Kummerlaender <adrian@kummerlaender.eu>
2024-06-28 16:58:20 +0200

    Update to 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "System environment of Adrian Kummerlaender";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-24.05;
     pkgs-personal.url = github:KnairdA/pkgs/master;
   };
 
